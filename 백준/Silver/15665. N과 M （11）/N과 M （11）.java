458325e6494be2d84ad56dc653b189e033956eb1
min7672 <59133075+min7672@users.noreply.github.com>
2025-06-05 21:11:17 +0900

    [Silver II] Title: N과 M (11), Time: 1324 ms, Memory: 317328 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+import java.util.StringTokenizer;
+
+public class Main {
+        static int arr[];
+        static ArrayList<Integer> node;
+        static int N;
+        static int M;
+        static boolean visited[];
+        static ArrayList<Integer> result;
+    static void permu(int idx,BufferedWriter bw)throws IOException{
+        
+        if(idx==M){
+            bw.write(result.toString().replaceAll(",", "").replace("[", "").replace("]", "")+"\n");
+            return;
+        }
+        for(int i=0;i<node.size();i++){
+            int tmp=node.get(i);
+            
+            result.add(tmp);
+            permu(idx+1, bw);
+            result.remove(result.size()-1);
+            
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        String info[]=br.readLine().split(" ");
+        N=Integer.parseInt(info[0]);
+        M=Integer.parseInt(info[1]);
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        arr=new int[10001];
+        node=new ArrayList<>();
+        for(int i=0;i<N; i++){
+            int tmp=Integer.parseInt(st.nextToken());
+            if(arr[tmp]==0){
+                node.add(tmp);
+            }
+            arr[tmp]+=1;
+
+
+        }
+        node.sort(null);
+        result=new ArrayList<>();
+        visited=new boolean[N];
+        permu(0,bw);
+        bw.flush();
+    }
+
+}
