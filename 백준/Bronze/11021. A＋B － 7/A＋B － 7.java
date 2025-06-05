68d37ee56fc1d023c4fb6625181110f5620831b9
min7672 <59133075+min7672@users.noreply.github.com>
2025-06-05 21:53:43 +0900

    [Bronze V] Title: A+B - 7, Time: 200 ms, Memory: 18836 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t= Integer.parseInt(sc.nextLine());
+        
+        for(int i=1; i<t+1;i++){
+            String []tmp=sc.nextLine().split(" ");
+            
+            System.out.println("Case #"+i+": "+(Integer.parseInt(tmp[0])+Integer.parseInt(tmp[1])));
+        }
+    }
+}
