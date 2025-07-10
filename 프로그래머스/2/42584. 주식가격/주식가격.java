cc9eefcfa4e4f1e6abb70df8e6cb900db3a52004
min7672 <59133075+min7672@users.noreply.github.com>
2025-07-10 17:46:09 +0900

    [level 2] Title: 주식가격, Time: 13.79 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+class Solution {
+    
+    public int[] solution(int[] prices) {
+              int answer[]=new int[prices.length];
+        for(int i=0;i<prices.length;i++){
+            int cur=prices[i];
+            int cnt=0;
+            for(int j=i+1;j<prices.length;j++){
+                cnt++;
+                if(prices[j]<cur){
+                    break;
+                }
+            }
+            answer[i]=cnt;
+        }
+        return answer;
+    }
+}
