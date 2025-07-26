170104410c90fc9d034d1c3ed63247b68e9c5599
min7672 <59133075+min7672@users.noreply.github.com>
2025-07-26 23:38:22 +0900

    [Silver III] Title: N과 M (1), Time: 212 ms, Memory: 24108 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,51 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main {
+public class Main{
     static int N;
     static int M;
-    static boolean visited[];
+    static boolean[] visited;
     static StringBuilder sb;
-    static void permu(int idx){
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException{
+        init();
+        solve();
+    }
+    static void init() throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String input[]=new String[2];
+        input=br.readLine().split(" ");
+        br.close();
+        N=Integer.parseInt(input[0]);
+        M=Integer.parseInt(input[1]);
+        visited=new boolean[N];
+        sb=new StringBuilder();
+        bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+    static void solve()throws IOException{
+        permu(0);
+        bw.flush();
+        bw.close();
+    }
+    static void permu(int idx)throws IOException{
+        //종료조건
         if(idx==M){
-            System.out.println(sb);
+            bw.write(sb.toString());
+            bw.newLine();
             return;
         }
-        
-        
         for(int i=0;i<N;i++){
             if(visited[i])continue;
             visited[i]=true;
-            sb.append((i+1)+" ");
-
+            sb.append(i+1).append(" ");
             permu(idx+1);
-            sb.deleteCharAt(sb.length()-1);
-            sb.deleteCharAt(sb.length()-1);
             visited[i]=false;
-            
+            sb.delete(sb.length()-2, sb.length());
         }
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String info[]=br.readLine().split(" ");
-        N=Integer.parseInt(info[0]);
-        M=Integer.parseInt(info[1]);
-        visited=new boolean[N];
-        sb=new StringBuilder();
-        permu(0);
 
     }
 }
